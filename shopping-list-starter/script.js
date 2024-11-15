f8bc3f7ea8fbc9b67baf05c848678a4b9e5318a4
marina-terina <marinat@outlook.com.au>
2024-11-15 21:26:32 +1000

    created shopping list with add and clear functionality

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,7 +1,42 @@
-const addItem = () => {
+let shoppingListItems = ["milk", "eggs", "bread"];
 
+ // Here we grab the ul from the HTML
+let listElement = document.getElementById("shopping-list-items");
+
+for (const shoppingItem of shoppingListItems) {
+console.log(shoppingItem);
+
+// We create a list element
+let itemElement = document.createElement("li");
+
+ // Add the inner text to the list element
+itemElement.innerText = shoppingItem;
+
+ // Add the list element to the ul
+listElement.appendChild(itemElement);
+}
+const addItem = () => {
+    let item = document.getElementById("new-item-text").value;
+    shoppingListItems = [...shoppingListItems, item];
+    updateItems();
+    document.getElementById("new-item-text").value = "";
 };
 
 const updateItems = () => {
+     // First we get the list element
+    let listElement = document.getElementById("shopping-list-items");
+     // Then we clear it of any existing items
+    listElement.innerHTML = "";
+    
+     // Then we loop through the shopping list items and add them to the list
+    for (const shoppingItem of shoppingListItems) {
+    let itemElement = document.createElement("li");
+    itemElement.innerText = shoppingItem;
+    listElement.appendChild(itemElement);
+    }
+};
 
+const clearList = () => {
+    shoppingListItems = [];
+    updateItems(); 
 };
